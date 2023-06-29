86257e895d0c5e0bc0d17cea999653dc91968fd3
Duff Neubauer <d.neubauer@me.com>
2023-06-29 15:04:35 -0700

    Initial Commit

---
+++ Triathlon/TriathlonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriathlonApp.swift
+//  Triathlon
+//
+//  Created by Duff Neubauer on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct TriathlonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
